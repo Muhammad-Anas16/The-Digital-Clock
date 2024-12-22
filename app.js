5889f7abf0d4bfabb821069b656e16d1eb60b6a2
muhammad-anas16 <anas.hanf38@gmail.com>
2024-12-22 13:06:34 +0500

    update

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
 
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+
 setInterval(() => {
 
     const main = new Date();
-    hrs.innerHTML = main.getHours();
+
+    hrs.innerHTML = main.getHours() - 12;
     min.innerHTML = main.getMinutes();
     sec.innerHTML = main.getSeconds();
-
     day.innerHTML = daysOfWeek[main.getDay()];
 
     date.innerText = `${months[main.getMonth()]} : ${main.getDate()} : ${main.getFullYear() - 2000}`;
