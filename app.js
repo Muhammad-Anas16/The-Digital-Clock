a5dd5495308014f8f96926873c8756a5d2142d7d
muhammad-anas16 <anas.hanf38@gmail.com>
2024-12-22 12:55:49 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const hrs = document.getElementById("Hrs");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+const day = document.getElementById("day");
+const date = document.getElementById("theDate");
+
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+setInterval(() => {
+
+    const main = new Date();
+    hrs.innerHTML = main.getHours();
+    min.innerHTML = main.getMinutes();
+    sec.innerHTML = main.getSeconds();
+
+    day.innerHTML = daysOfWeek[main.getDay()];
+
+    date.innerText = `${months[main.getMonth()]} : ${main.getDate()} : ${main.getFullYear() - 2000}`;
+
+}, 1000);
